b74e14dc165bc827af3ae39a00f513b3b384664a
Ben-jpeg <72921912+Ben-jpeg@users.noreply.github.com>
2020-11-27 17:05:33 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/93585804ff.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="site3.css">
+
+    <title></title>
+</head>
+
+
+<body>
+
+
+    <div class="headerbis border flex spacearound">
+
+        <div class="logo">HALLO/win</div>
+
+        <nav class="flex spacebetween ">
+            <ul class="points flex ">
+                <li> <a href="">Accueil</a> </li>
+                <li> <a class="blk" href="#"> Thèmes</a></li>
+                <li> <a class="blk" href="#"> Reseaux</a> </li>
+                <li> <a class="blk" href="#"> Personalisation</a></li>
+                <li> <a class="blk" href="#"> Extension</a></li>
+                <li> <a class="blk" href="#"> Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <header>
+
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="hal1.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>LA CITROUILLE</h5>
+                        <p> Ca fonctionne bien si la casserole est sale. Ca conserve bien les plats chauds en plus !</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="hal2.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>SAUCE SOJA</h5>
+                        <p>Non jrigole, je sais pas ce que c'est mais ya une chauve souris sur l'emballage, ça m'as pas trop l'air safe.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="hal3.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>?!$</h5>
+                        <p>C'etait le jour où j'ai revu le css du tout premier projet qu'on a fait. Il avait muté depuis la dernière fois, c'etait horrible</p>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+
+    </header>
+
+
+    <section1 class="flex spacearound margintop marginbot">
+
+        <article class="textalign">
+            <div><i class="fas fa-magic fontsize46px"></i></div>
+            <div>
+                <h4>Personnalisation</h4>
+            </div>
+            <div>
+                <p>Des thèmes et simple</br>+ de 200 propositions différentes </p>
+            </div>
+        </article>
+
+
+        <article class="textalign">
+            <div><i class="fas fa-gift fontsize46px"></i></div>
+            <div>
+                <h4>Police d'écriture</h4>
+            </div>
+            <div>
+                <p>Avec GoogleFonts et</br>FontAwesome icons </p>
+            </div>
+        </article>
+
+
+        <article class="textalign">
+            <div><i class="fas fa-laptop fontsize46px"></i></div>
+            <div>
+                <h4>Responsive</h4>
+            </div>
+            <div>
+                <p>Fait avec Bootstrap 3,</br>HTML5 and CSS3 </p>
+            </div>
+
+        </article>
+
+
+        <article class="textalign">
+            <div><i class="fas fa-puzzle-piece fontsize46px"></i></div>
+            <div>
+                <h4>Extensions</h4>
+            </div>
+            <div>
+                <p>Profitez de tas de thèmes gratuitement </br>Tout cela sans limites </p>
+            </div>
+        </article>
+
+    </section1>
+
+
+    <section2 class="flex flexcol textalign juscenter border margin ">
+        <h3 class="char mrg">UTILISEZ UN THÈME POUR VOTRE PROCHAIN SITE !</h3>
+
+
+        <p class="litt">Ces thèmes personnalisable et disponible gratuitement offrent une possibilité de personalisation infinie ! Profitez-en !</p>
+
+        <div class="flex textalign juscenter">
+            <button class="button">TÉLÉCHARGER</button></div>
+    </section2>
+
+
+
+
+
+
+
+    <footer></footer>
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+
+</html>
